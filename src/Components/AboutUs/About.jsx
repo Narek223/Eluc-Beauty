744a137d0be075ebb3da51f48a37d9b7af25ac72
Narek223 <panosyan-narek@mail.ru>
2025-06-09 20:51:35 +0400

    about

--- src/Components/AboutUs/About.jsx
+++ src/Components/AboutUs/About.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styles from "./about.module.scss";
 import PathTrace from "../../SharedComponents/PathTrace/PathTrace";
+import abotimg from "../../assets/About/Image.png";
+import { count } from "../../Services/Date/About/counter";
 export default function About() {
   return (
     <div className={styles.AboutComponent}>
@@ -9,8 +11,46 @@ export default function About() {
         nextitle={"AwesomeBeauty salon"}
         oldPage={"Home"}
         currentPage={"About"}
-        
       />
+      <div className={styles.aboutContent}>
+        <div className={styles.aboutWrapper}>
+          <div className={styles.img}>
+            <img src={abotimg} />
+          </div>
+          <div className={styles.text}>
+            <h1> FULL RANGE OF SERVICES FROM ELUX </h1>
+            <p>
+              EluX Beauty Salon is a premier beauty destination founded on the
+              principles of luxury, innovation, and personalized care. Our team
+              of experienced professionals is passionate about helping you look
+              and feel your best.
+            </p>
+            <p>
+              At EluX Beauty Salon, our mission is to deliver a premium beauty
+              experience that combines high-quality products, exceptional
+              service, and a serene atmosphere. We are committed to creating a
+              space where you can relax, rejuvenate, and transform your look.
+            </p>
+            <p>
+              EluX Beauty Salon is a premier beauty destination founded on the
+              principles of luxury, innovation, and personalized care. Our team
+              of experienced professionals is passionate about helping you look
+              and feel your best.
+            </p>
+          </div>
+        </div>
+        <div className={styles.counterWrapper}>
+          {count.map((item) => {
+            return (
+              <div key={item.id} className={styles.counter}>
+                <h1>{item.title}</h1>
+                <h2>{item.number}</h2>
+                <p>{item.text}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/Services/Date/About/counter.js
@@ -0,0 +1,26 @@
+ export const count = [
+    {
+      id: 1,
+      title: "Years of Experience",
+      number: "10+",
+      text: "Hair Extensions",
+    },
+    {
+      id: 2,
+      title: "Quality Assurance",
+      number: "99.9%",
+      text: "Top-notch Services",
+    },
+    {
+      id: 3,
+      title: "Satisfied Clients",
+      number: "1.5 M",
+      text: "Happy Customers",
+    },
+    {
+      id: 4,
+      title: "Professional Staff",
+      number: "500",
+      text: "Certified Experts",
+    },
+  ];
