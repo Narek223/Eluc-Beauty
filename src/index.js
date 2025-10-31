705835226273be1037e9c65133f3e3c62d0400b0
Narek223 <panosyan-narek@mail.ru>
2025-10-31 13:09:22 +0400

    fix: use HashRouter for GitHub Pages

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from "react-redux";
 import{ store} from "./Redux/Store/store";
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter ,HashRouter} from 'react-router-dom';
 import './i18n.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
  
   </Provider>
 );
